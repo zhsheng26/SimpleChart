b55254ccbf0f3445cf9058a48035a68e4c1e3ea9
zhsheng26 <zhsheng26@126.com>
2016-11-27 21:21:27 +0800

    init

--- app/src/main/java/com/wedotech/simplechart/PathActivity.java
+++ app/src/main/java/com/wedotech/simplechart/PathActivity.java
@@ -1,9 +1,12 @@
 package com.wedotech.simplechart;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.wedotech.simplechart.view.PathTestView;
 
@@ -21,12 +24,23 @@ public class PathActivity extends AppCompatActivity {
         setContentView(R.layout.activity_path);
         final PathTestView pathTestView = (PathTestView) findViewById(R.id.pathView);
         btnReset = findViewById(R.id.btn_reset);
+        final ImageView imageView = (ImageView) findViewById(R.id.iv);
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 pathTestView.reset();
             }
         });
+        View save = findViewById(R.id.btn_save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pathTestView.saveToGallery("path", "path", "path", Bitmap.CompressFormat.JPEG, 100);
+                Bitmap bitmap = BitmapFactory.decodeFile(pathTestView.getPath());
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/wedotech/simplechart/view/PathTestView.java
+++ app/src/main/java/com/wedotech/simplechart/view/PathTestView.java
@@ -1,16 +1,25 @@
 package com.wedotech.simplechart.view;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by YangYan on 2016/11/27.
@@ -24,6 +33,7 @@ public class PathTestView extends View {
     private Path mPath = new Path();
     private float mPreX;
     private float mPreY;
+    private String mFilePath;
 
     public PathTestView(Context context) {
         super(context);
@@ -123,4 +133,96 @@ public class PathTestView extends View {
 
         canvas.drawPath(path,paint);
     }*/
+
+    public boolean saveToGallery(String fileName, String subFolderPath, String fileDescription, Bitmap.CompressFormat
+            format, int quality) {
+        // 控制图片质量
+        if (quality < 0 || quality > 100)
+            quality = 50;
+
+        long currentTime = System.currentTimeMillis();
+
+        File extBaseDir = Environment.getExternalStorageDirectory();
+        File file = new File(extBaseDir.getAbsolutePath() + "/DCIM/" + subFolderPath);
+        if (!file.exists()) {
+            if (!file.mkdirs()) {
+                return false;
+            }
+        }
+
+        String mimeType = "";
+        switch (format) {
+            case PNG:
+                mimeType = "image/png";
+                if (!fileName.endsWith(".png"))
+                    fileName += ".png";
+                break;
+            case WEBP:
+                mimeType = "image/webp";
+                if (!fileName.endsWith(".webp"))
+                    fileName += ".webp";
+                break;
+            case JPEG:
+            default:
+                mimeType = "image/jpeg";
+                if (!(fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")))
+                    fileName += ".jpg";
+                break;
+        }
+
+        mFilePath = file.getAbsolutePath() + "/" + fileName;
+        FileOutputStream out;
+        try {
+            out = new FileOutputStream(mFilePath);
+
+            Bitmap b = getChartBitmap();
+            b.compress(format, quality, out);
+
+            out.flush();
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return false;
+        }
+
+        long size = new File(mFilePath).length();
+
+        ContentValues values = new ContentValues(8);
+
+        // store the details
+        values.put(MediaStore.Images.Media.TITLE, fileName);
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, fileName);
+        values.put(MediaStore.Images.Media.DATE_ADDED, currentTime);
+        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType);
+        values.put(MediaStore.Images.Media.DESCRIPTION, fileDescription);
+        values.put(MediaStore.Images.Media.ORIENTATION, 0);
+        values.put(MediaStore.Images.Media.DATA, mFilePath);
+        values.put(MediaStore.Images.Media.SIZE, size);
+
+        return getContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values) != null;
+    }
+
+    private Bitmap getChartBitmap() {
+        // 创建一个bitmap 根据我们自定义view的大小
+        Bitmap returnedBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.RGB_565);
+        // 绑定canvas
+        Canvas canvas = new Canvas(returnedBitmap);
+        // 获取视图的背景
+        Drawable bgDrawable = getBackground();
+        if (bgDrawable != null)
+            // 如果有就绘制
+            bgDrawable.draw(canvas);
+        else
+            // 没有就绘制白色
+            canvas.drawColor(Color.WHITE);
+        // 绘制
+        draw(canvas);
+        return returnedBitmap;
+    }
+
+    public String getPath() {
+        return mFilePath;
+    }
 }
